606e4d4201c6b8b5ea5489c91bebb3c529df50cf
Toxcastel <a.castelr@gmail.com>
2022-11-17 17:49:05 -0300

    added todoItem

--- src/Todo/components/TodoItem.tsx
+++ src/Todo/components/TodoItem.tsx
@@ -1,6 +1,17 @@
+/* Nuestro componente recibirá por defecto un argumento, el cual estamos destructurando aquí como {todo}.
+    Al dejar solo {todo}, typescript nos arroja error porque define automáticamente como 'any', cosa que se debe evitar
+    Para evitarlo, debemos definir el tipado de lo que este componente recibirá, por lo tanto, crearemos aquí una interface */
 
-export const TodoItem = () => {
-  return (
-    <div></div>
-  )
+import { Todo } from "../interfaces/interfaces";
+
+// el uso de interfaces tiene menos carga de código que type
+interface props {
+    todo: Todo;
 }
+
+export const TodoItem = ({ todo }: props) => {
+    const handleDbClick = () => {
+        console.log("handleDbClick: ", todo.desc);
+    };
+    return <li onDoubleClick={handleDbClick}>{todo.desc}</li>;
+};
